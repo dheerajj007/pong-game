14e63c291318364f19ca2c7f0a0f603945d68505
fixxtion <dheerajkumarr235@gmail.com>
2021-04-19 21:20:23 +0530

    Ball Class created

---
+++ ball.py
@@ -0,0 +1,18 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+
+    def refresh(self):
+        self.goto(380, 280)
+    
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
+
